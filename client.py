ab27df3f9e50e195d68877870bbfe33a8ded41cb
GustavoBMendes <mendes.gustavob@gmail.com>
2020-09-23 15:57:18 -0300

    creating a blockchain

---
+++ client.py
@@ -0,0 +1,31 @@
+'''
+title           : client.py
+description     : A blockchain client implemenation, with the following features
+                  - Wallets generation using Public/Private key encryption (based on RSA algorithm)
+                  - Generation of transactions with RSA encryption      
+author          : Gustavo Belançon Mendes
+date_created    : 20180212
+date_modified   : 20180309
+version         : 1.0
+usage           : python client.py
+                  python client.py -p 8080
+                  python client.py --port 8080
+python_version  : 3.6.1
+Comments        : Wallet generation and transaction signature is based on [1]
+References      : [1] https://github.com/julienr/ipynb_playground/blob/master/bitcoin/dumbcoin/dumbcoin.ipynb
+'''
+
+from collections import OrderedDict
+
+import binascii
+
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+import requests
+from flask import Flask, jsonify, request, render_template
+
+
